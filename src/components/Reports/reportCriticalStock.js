c086c4cbcbb149ac1add863e021d9e26c5e6b077
Luca Trejo <luca.trejo@mercadolibre.com>
2020-12-17 01:56:08 -0300

    fix reports

--- src/components/Reports/reportCriticalStock.js
+++ src/components/Reports/reportCriticalStock.js
@@ -1,6 +1,6 @@
 import jsPDF from "jspdf";
 import "jspdf-autotable";
-
+import logo from "assets/img/logo_zion.jpeg";
 
 const generatePDFCriticalStock = items => {
   const doc = new jsPDF();
@@ -12,17 +12,24 @@ const generatePDFCriticalStock = items => {
     const reportData = [
       item.id,
       item.name,
-      item.stock,
-      item.critical_stock
+      item.stock + " u.",
+      item.critical_stock + " u."
     ];
     tableRows.push(reportData);
   });
 
+  doc.addImage(logo, "JPEG", 5, 10, 50, 25);
 
-  doc.autoTable(tableColumn, tableRows, { startY: 20 });
+  doc.autoTable(tableColumn, tableRows, { startY: 55 });
   const date = Date().split(" ");
   const dateStr = date[2] + date[3];
-  doc.text("Artículos con stock por debajo del punto crítico", 14, 15);
+  doc.text("Artículos con stock por debajo del punto crítico", 50, 23);
+
+    doc.setFontSize(10);
+    doc.text("Av. Provincial de Vialidad 1320, Santiago del Estero", 18, 35);
+    doc.text("Tel: 3856300234 - 3854601341", 18, 40);
+    doc.text("Email: zion@gmail.com", 18, 45);
+
   doc.save(`report_${dateStr}.pdf`);
 };
 
--- src/components/Reports/reportPurchasesEmployee.js
+++ src/components/Reports/reportPurchasesEmployee.js
@@ -1,6 +1,6 @@
 import jsPDF from "jspdf";
 import "jspdf-autotable";
-
+import logo from "assets/img/logo_zion.jpeg";
 
 const generatePDFPurchasesEmployee = items => {
   const doc = new jsPDF();
@@ -15,8 +15,8 @@ const generatePDFPurchasesEmployee = items => {
           item.employee_last_name + ", " + item.employee_name,
           item.date,
           detail.name,
-          detail.quantity,
-          detail.unit_price
+          detail.quantity + " u.",
+          "$ " + detail.unit_price
       ];
 
     tableRows.push(reportData);
@@ -24,11 +24,17 @@ const generatePDFPurchasesEmployee = items => {
     });
   });
 
+  doc.addImage(logo, "JPEG", 5, 10, 50, 25);
 
-  doc.autoTable(tableColumn, tableRows, { startY: 20 });
+  doc.autoTable(tableColumn, tableRows, { startY: 55 });
   const date = Date().split(" ");
   const dateStr = date[2] + date[3];
-  doc.text("Reporte de Compras del mes por Empleado", 14, 15);
+  doc.text("Reporte de Compras del mes por Empleado", 50, 23);
+
+  doc.setFontSize(10);
+  doc.text("Av. Provincial de Vialidad 1320, Santiago del Estero", 18, 35);
+  doc.text("Tel: 3856300234 - 3854601341", 18, 40);
+  doc.text("Email: zion@gmail.com", 18, 45);
   doc.save(`report_${dateStr}.pdf`);
 };
 
